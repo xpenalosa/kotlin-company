bfdea62b713472ef40d7d7eb13ef14743f6b91d6
xpenalosa <xavier.penalosa.esteller@gmail.com>
2020-09-22 20:30:57 +0200

    Create tests for City and remove Mockk dependency

--- src/main/kotlin/actors/City.kt
+++ src/main/kotlin/actors/City.kt
@@ -1,9 +1,16 @@
 package actors
 
+import java.time.Duration
 import kotlin.math.abs
 
-data class City(val id: Long, val name: String, val latitude: Int, val longitude: Int){
+data class City(val id: Long, val name: String, val latitude: Int, val longitude: Int) {
+
+    companion object {
+        var delayPerDistanceUnit: Duration = Duration.ofSeconds(1)
+
+        private fun getDeliveryForDistance(distance: Int): Int = (delayPerDistanceUnit.toMillis() * distance).toInt()
+    }
 
     fun getDistanceTo(other: City): Int = abs(longitude - other.longitude) + abs(latitude - other.latitude)
-    fun getDeliveryDelayTo(other: City): Int = if (this == other) 1 else getDistanceTo(other)
+    fun getDeliveryDelayTo(other: City): Int = getDeliveryForDistance(getDistanceTo(other))
 }
---
+++ src/test/kotlin/actors/CityTest.kt
@@ -0,0 +1,42 @@
+package actors
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.function.Executable
+
+internal class CityTest {
+
+    @Test
+    fun `getDistanceTo should return Manhattan distance between both cities using latitude and longitude`() {
+        val city1: City = City(1, "City1", 0, 0)
+        val city2: City = City(2, "City2", 1, 1)
+        val city3: City = City(3, "City3", 5, 1)
+
+        Assertions.assertAll(
+            Executable { Assertions.assertSame(2, city1.getDistanceTo(city2)) },
+            Executable { Assertions.assertSame(6, city1.getDistanceTo(city3)) },
+            Executable { Assertions.assertSame(4, city2.getDistanceTo(city3)) }
+        )
+    }
+
+    @Test
+    fun `getDistanceTo should be commutative`() {
+        val city1: City = City(1, "City1", 0, 0)
+        val city2: City = City(2, "City2", 5, 7)
+
+        Assertions.assertSame(city2.getDistanceTo(city1), city1.getDistanceTo(city2))
+    }
+
+    @Test
+    fun `getDeliveryDelayTo should linearly relate to distance`() {
+        val city1: City = City(1, "City1", 0, 0)
+        val city2: City = City(2, "City2", 1, 1)
+        val city3: City = City(3, "City3", 5, 1)
+
+        Assertions.assertAll(
+            Executable { Assertions.assertTrue(city1.getDeliveryDelayTo(city2) < city1.getDeliveryDelayTo(city3)) },
+            Executable { Assertions.assertSame(city1.getDeliveryDelayTo(city2), city2.getDeliveryDelayTo(city1)) },
+        )
+
+    }
+}
